4ab22a8691c2a5d92d564a978db8f2013cbbe216
Aliif Arief <aliif.arief.m@gmail.com>
2022-06-21 21:34:57 +0700

    feat: readme & database schema updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CRUD Katalog toko Buku 
+# Katalog toko Buku 
 
 Proyek akhir responsi praktikum ujian akhir semester 2 mata kuliah pemrograman web 1
 
@@ -8,8 +8,15 @@ Kelas : PL1AA
 NIM : 21/479029/SV/19418
 ```
 
+Fitur Website:
+
+- CRUD data buku kalau sudah Login
+- Live search by book title / author
+- login and register user
+- user access privileges
+
 Tech Stack : 
 
-- PHP Native 
-- MySQL, 
+- PHP 8 Native 
+- MariaDB 
 - TailwindCSS
--- sql/db_buku_uas.sql
+++ sql/db_buku_uas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 20, 2022 at 09:52 AM
+-- Generation Time: Jun 21, 2022 at 04:30 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -41,8 +41,31 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `book_name`, `book_description`, `book_author`, `book_price`, `book_picture`) VALUES
-(8, 'Buku Pertama edit', 'ini buku yang pertama setelah styling dengan tailwind CSS yang telah edited', 'Aliif am', 121000, '196-buku1.jpg'),
-(9, 'Hujan Hujanan', 'Tentang persahabatan\r\nTentang cinta\r\nTentang perpisahan\r\nTentang melupakan\r\nTentang hujan', 'Tere Liye', 78100, '407-hujan-tere.jpg');
+(8, 'Buku Pertama post Mavbar', 'ini buku yang pertama \'setelah styling dengan tailwind CSS yang telah edited fixed anjay udah aman dari admin 2', 'aliif admin2', 121000, '196-buku1.jpg'),
+(9, 'Kehujanan', 'Tentang persahabatan\r\nTentang cinta\r\nTentang perpisahan\r\nTentang melupakan\r\nTentang hujan', 'Barabara', 78100, '407-hujan-tere.jpg'),
+(10, 'The Pragmatic Programmer', 'ini buku ajib bener ente semua programmer wajib baca ini jangan ngoding mulu ente', 'David Thomas', 399000, '485-pragmatic-programmer.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `create_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `create_datetime`) VALUES
+(1, 'admin', 'admin@mail.com', '21232f297a57a5a743894a0e4a801fc3', '2022-06-21 09:37:06'),
+(2, 'admin2', 'admin2@mail.com', 'c84258e9c39059a89ab77d846ddab909', '2022-06-21 16:24:30');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +78,12 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +91,13 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
